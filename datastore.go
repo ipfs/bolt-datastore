970bfd3db452ea88caa18114be4720455e15386c
Steven Allen <steven@stebalien.com>
2019-09-24 15:56:41 -0700

    datastore: fix query logic
    We need to apply the query in the following order:
    
    * Filter
    * Order
    * Offset
    * Limit
    
    We were using the following order:
    
    * Offset
    * Limit
    * Filter
    * Order

--- datastore.go
+++ datastore.go
@@ -123,10 +123,19 @@ func (bd *BoltDatastore) PutMany(data map[ds.Key]interface{}) error {
 }
 
 func (bd *BoltDatastore) Query(q query.Query) (query.Results, error) {
+	// Special case order by key.
+	orders := q.Orders
+	if len(orders) > 0 {
+		switch q.Orders[0].(type) {
+		case query.OrderByKey, *query.OrderByKey:
+			// Already ordered by key.
+			orders = nil
+		}
+	}
+
 	qrb := query.NewResultBuilder(q)
 	qrb.Process.Go(func(worker goprocess.Process) {
 		bd.db.View(func(tx *bolt.Tx) error {
-
 			buck := tx.Bucket(bd.bucketName)
 			c := buck.Cursor()
 
@@ -135,34 +144,85 @@ func (bd *BoltDatastore) Query(q query.Query) (query.Results, error) {
 				prefix = []byte(qrb.Query.Prefix)
 			}
 
-			cur := 0
-			sent := 0
-			for k, v := c.Seek(prefix); k != nil; k, v = c.Next() {
-				if cur < qrb.Query.Offset {
-					cur++
-					continue
+			// If we need to sort, we'll need to collect all the
+			// results up-front.
+			if len(orders) > 0 {
+				// Query and filter.
+				var entries []query.Entry
+				for k, v := c.Seek(prefix); k != nil; k, v = c.Next() {
+					dk := ds.NewKey(string(k)).String()
+					e := query.Entry{Key: dk}
+					if !qrb.Query.KeysOnly {
+						// We copy _after_ filtering/sorting.
+						e.Value = v
+					}
+					if filter(q.Filters, e) {
+						continue
+					}
+					entries = append(entries, e)
 				}
-				if qrb.Query.Limit > 0 && sent >= qrb.Query.Limit {
-					break
-				}
-				dk := ds.NewKey(string(k)).String()
-				e := query.Entry{Key: dk}
 
-				if !qrb.Query.KeysOnly {
-					buf := make([]byte, len(v))
-					copy(buf, v)
-					e.Value = buf
+				// sort
+				query.Sort(orders, entries)
+
+				// offset/limit
+				entries = entries[qrb.Query.Offset:]
+				if qrb.Query.Limit > 0 {
+					if qrb.Query.Limit < len(entries) {
+						entries = entries[:qrb.Query.Limit]
+					}
 				}
 
-				select {
-				case qrb.Output <- query.Result{Entry: e}: // we sent it out
-					sent++
-				case <-worker.Closing(): // client told us to end early.
-					break
+				// Send
+				for _, e := range entries {
+					// Copy late so we don't have to copy
+					// values we don't use.
+					e.Value = append(e.Value[0:0:0], e.Value...)
+					select {
+					case qrb.Output <- query.Result{Entry: e}:
+					case <-worker.Closing(): // client told us to end early.
+						return nil
+					}
 				}
-				cur++
+			} else {
+				// Otherwise, send results as we get them.
+				offset := 0
+				for k, v := c.Seek(prefix); k != nil; k, v = c.Next() {
+					dk := ds.NewKey(string(k)).String()
+					e := query.Entry{Key: dk, Value: v}
+					if !qrb.Query.KeysOnly {
+						// We copy _after_ filtering.
+						e.Value = v
+					}
+
+					// pre-filter
+					if filter(q.Filters, e) {
+						continue
+					}
+
+					// now count this item towards the results
+					offset++
+
+					// check the offset
+					if offset < qrb.Query.Offset {
+						continue
+					}
+
+					e.Value = append(e.Value[0:0:0], e.Value...)
+					select {
+					case qrb.Output <- query.Result{Entry: e}:
+						offset++
+					case <-worker.Closing():
+						return nil
+					}
+
+					if qrb.Query.Limit > 0 &&
+						offset >= (qrb.Query.Offset+qrb.Query.Limit) {
+						// all done.
+						return nil
+					}
+				}
 			}
-
 			return nil
 		})
 	})
@@ -170,13 +230,17 @@ func (bd *BoltDatastore) Query(q query.Query) (query.Results, error) {
 	// go wait on the worker (without signaling close)
 	go qrb.Process.CloseAfterChildren()
 
-	qr := qrb.Results()
-	for _, f := range q.Filters {
-		qr = query.NaiveFilter(qr, f)
-	}
+	return qrb.Results(), nil
+}
 
-	qr = query.NaiveOrder(qr, q.Orders...)
-	return qr, nil
+// filter checks if we should filter out the query.
+func filter(filters []query.Filter, entry query.Entry) bool {
+	for _, filter := range filters {
+		if !filter.Filter(entry) {
+			return true
+		}
+	}
+	return false
 }
 
 type boltBatch struct {
